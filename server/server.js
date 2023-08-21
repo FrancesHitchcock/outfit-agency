6b5ce071db86244381872fffc3b27281a4c7073e
Frances <fran.h.hitchcock@gmail.com>
2023-08-21 17:34:38 +0100

    add http methods to server.js

--- server/server.js
+++ server/server.js
@@ -19,6 +19,53 @@ app.get("/", (request, response) => {
   response.status(200).json("root route testing message");
 });
 
+app.get("/outfits", async (request, response) => {
+  try {
+    const allOutfits = await Outfit.find(request.query);
+    response.status(200).json(allOutfits);
+  } catch (error) {
+    response.status(404).json(error);
+  }
+});
+
+app.get("/outfits/:id", async (request, response) => {
+  try {
+    const outfitById = await Outfit.find({
+      _id: request.params.id,
+    });
+    response.status(200).json(outfitById);
+  } catch (error) {
+    response.status(404).json(error);
+  }
+});
+
+app.post("/outfits", async (request, response) => {
+  try {
+    const newOutfit = await Outfit.create(request.body);
+    response.status(204).json(newOutfit);
+  } catch (error) {
+    response.status(404).json(error);
+  }
+});
+
+app.put("/outfits/:id", async (request, response) => {
+  try {
+    await Outfit.findByIdAndUpdate(request.params.id, request.body);
+    response.status(200).send();
+  } catch (error) {
+    response.send(error);
+  }
+});
+
+app.delete("/outfits/:id", async (request, response) => {
+  try {
+    const deletedOutfit = await Outfit.findByIdAndDelete(request.params.id);
+    response.status(200).send(deletedOutfit);
+  } catch (error) {
+    response.status(500).json(error);
+  }
+});
+
 app.listen(PORT, () => console.log(`app is listening on port: ${PORT}`));
 
 // mongoDB cluster password: DkkyAVlzJTOzDezP
