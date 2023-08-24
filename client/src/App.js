fb14d9f5d60cc3f6e95a247936538f4ce76d7c69
Frances Hitchcock <fran.h.hitchcock@gmail.com>
2023-08-24 18:18:12 +0100

    add link to server

--- client/src/App.js
+++ client/src/App.js
@@ -7,20 +7,16 @@ import Outfits from "./pages/Outfits";
 import Admin from "./pages/Admin";
 import Home from "./pages/Home";
 
+// Render url: https://outfit-agency.onrender.com
+// Local url: http://localhost:8080
+
 function App() {
   const [allOutfits, setAllOutfits] = useState([]);
   const [filterTerm, setFilterTerm] = useState("");
-  // const [addFormData, setAddFormData] = useState({
-  //   itemName: "",
-  //   ownerName: "",
-  //   colours: [],
-  //   cost: 0,
-  //   description: "",
-  // });
 
   async function getAllOutfits() {
     try {
-      const URL = "http://localhost:8080/outfits";
+      const URL = "https://outfit-agency.onrender.com/outfits";
       const resp = await axios.get(URL);
       setAllOutfits(resp.data);
     } catch (error) {
@@ -34,7 +30,7 @@ function App() {
 
   async function handleDeleteOutfit(id) {
     try {
-      const URL = `http://localhost:8080/outfits/${id}`;
+      const URL = `https://outfit-agency.onrender.com/outfits/${id}`;
       await axios.delete(URL);
       getAllOutfits();
     } catch (error) {
@@ -45,7 +41,7 @@ function App() {
   async function handleEditFormSubmit(event, id, outfit) {
     event.preventDefault();
     try {
-      const URL = `http://localhost:8080/outfits/${id}`;
+      const URL = `https://outfit-agency.onrender.com/outfits/${id}`;
       await axios.put(URL, outfit);
       getAllOutfits();
     } catch (error) {
@@ -55,31 +51,14 @@ function App() {
 
   async function handleAddFormSubmit(e, outfit) {
     e.preventDefault();
-    const URL = "http://localhost:8080/outfits";
+    const URL = "https://outfit-agency.onrender.com/outfits";
     await axios.post(URL, outfit);
     getAllOutfits();
-    // setAddFormData({
-    //   itemName: "",
-    //   ownerName: "",
-    //   colours: [],
-    //   cost: 0,
-    //   description: "",
-    // });
   }
 
   function handleFilterChange(e) {
     setFilterTerm(e.target.value);
   }
-
-  // function handleChange(event) {
-  //   const { name, value } = event.target;
-  //   setAddFormData((prevFormData) => {
-  //     return {
-  //       ...prevFormData,
-  //       [name]: name === "colours" ? value.split(", ") : value,
-  //     };
-  //   });
-  // }
 
   return (
     <div className="App">
